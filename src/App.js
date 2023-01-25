106ae9ab64dcb49f2dcc71f22db0436f5fc6dab6
Sunggyu Lee <sunggyu.lee@makebot.ai>
2023-01-25 17:56:30 +0900

    상품 데이터 추가 react-router-dom 추가

--- src/App.js
+++ src/App.js
@@ -1,10 +1,44 @@
-import { Button } from 'react-bootstrap';
+import { useState } from 'react';
+import { Navbar, Container, Nav, Row } from 'react-bootstrap';
 import './App.css';
+import data from './data.js';
+import Card from './Card';
+import { Routes, Route, Link } from 'react-router-dom';
 
 function App() {
+
+  const [shoes] = useState(data);
+
   return (
     <div className="App">
-      <Button variant='primary'>Primary</Button>
+
+      <Routes>
+        <Route path='/detail' element={<div>detail</div>} />{/* page */}
+        <Route path='/about' element={<div>about</div>}/>
+      </Routes>
+
+      <Navbar bg="light" expand="lg">
+        <Container>
+          <Navbar.Brand href="#home">ShoeShop</Navbar.Brand>
+          <Nav className="me-auto">
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#features">Cart</Nav.Link>
+          </Nav>
+        </Container>
+      </Navbar>
+      <div>
+        <img src={process.env.PUBLIC_URL + '/bg.png'} alt='main' height="300px" width="100%"/> {/* process.env.PUBLIC_URL 사이트 경로 */}
+      </div>
+      {/* <div className='main-big' style={{ backgroundImage : 'url(import 한 이름)'}}></div> css가 아닌 컴포넌트에서 사진 가져오는 방법*/}
+      <Row>
+        {
+          shoes.map((shoe) => {
+            return (
+              <Card id={ shoe.id } src={ shoe.src } title={ shoe.title } content={ shoe.content } price={ shoe.price }/>
+            )
+          })
+        }
+      </Row>
     </div>
   );
 }
---
+++ src/data.js
@@ -0,0 +1,27 @@
+let data = [
+  {
+    id : 0,
+    src: '/shoes1.jpg',
+    title : "White and Black",
+    content : "Born in France",
+    price : 120000
+  },
+
+  {
+    id : 1,
+    src: '/shoes2.jpg',
+    title : "Red Knit",
+    content : "Born in Seoul",
+    price : 110000
+  },
+
+  {
+    id : 2,
+    src: '/shoes3.jpg',
+    title : "Grey Yordan",
+    content : "Born in the States",
+    price : 130000
+  }
+];
+
+export default data;
