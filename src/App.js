7fe5c3edeb5a371b0f4c456039cc5ab994d2af76
LSGyu <thegathe17@gmail.com>
2023-01-25 12:34:40 +0900

    react-bootstrap 추가

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
+import { Button } from 'react-bootstrap';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
+      <Button variant='primary'>Primary</Button>
     </div>
   );
 }
